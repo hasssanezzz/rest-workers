5e2e962448cd5c9d16496975a1a9568c3f7a6ad9
Hassan Ezz <dhassanezz98@gmail.com>
2024-04-29 15:36:48 +0300

    feat: used flags

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hasssanezzz/rest-workers/api"
 )
 
 func main() {
-	listenAddr := ":3000"
-	server := api.NewServer(listenAddr, 5)
+	listenAddr := flag.String("a", ":3030", "the listen address in which the server will listen to")
+	workerCount := flag.Int("w", 5, "number of workers")
+	flag.Parse()
+
+	server := api.NewServer(*listenAddr, *workerCount)
 	server.Start()
 }
