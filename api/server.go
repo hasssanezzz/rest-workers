16d8fe0765f9da2824e7c60ccd1a6330bcfbadcb
Hassan Ezz <dhassanezz98@gmail.com>
2024-04-29 06:46:52 +0300

    feat: added get & list routes

--- api/server.go
+++ api/server.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/hasssanezzz/rest-workers/storage"
@@ -29,11 +30,38 @@ func NewServer(listenAddr string) *Server {
 func (s *Server) Start() error {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", s.handleHelloWorld).Methods("GET")
+	router.HandleFunc("/api/v0/task", s.handleListTasks).Methods("GET")
+	router.HandleFunc("/api/v0/task/{id}", s.handleGetTask).Methods("GET")
+	router.HandleFunc("/api/v0/task", s.handleCreateTask).Methods("POST")
+	router.HandleFunc("/api/v0/task/{id}", s.handleDeleteTask).Methods("DELETE")
 
 	return http.ListenAndServe(s.listenAddr, router)
 }
 
-func (s *Server) handleHelloWorld(w http.ResponseWriter, r *http.Request) {
-	WriteJSON(w, 200, "Hello world")
+func (s *Server) handleListTasks(w http.ResponseWriter, r *http.Request) {
+	tasks := s.storage.List()
+	WriteJSON(w, 200, tasks)
 }
+
+func (s *Server) handleGetTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		// TODO handle error: invalid id
+		WriteJSON(w, 400, "bad ID")
+		return
+	}
+
+	tasks, err := s.storage.Get(taskId)
+	if err != nil {
+		// TODO handle error: 404
+		WriteJSON(w, 404, "task not found")
+		return
+	}
+
+	WriteJSON(w, 200, tasks)
+}
+
+func (s *Server) handleCreateTask(w http.ResponseWriter, r *http.Request) {}
+
+func (s *Server) handleDeleteTask(w http.ResponseWriter, r *http.Request) {}
